0613f0dd4737df81c20d6d53bc7dd37e4bdd2597
marbokeeffe <mark.okeeffe@gmail.com>
2024-10-15 17:06:40 +0100

    Config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    crossOrigin: 'anonymous',
     images: {
         dangerouslyAllowSVG: true,
         contentDispositionType: 'attachment',
         contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    }
+    },
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+      },
 };
 
 export default nextConfig;
