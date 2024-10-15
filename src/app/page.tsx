b444ff2a634513cbc19376cb9974b4f3823a4f42
marbokeeffe <mark.okeeffe@gmail.com>
2024-10-15 10:57:45 +0100

    Typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,7 +48,7 @@ export default function Home() {
               <Image
                 src="/genealogy.svg"
                 alt="Genealoy"
-                className="lg;mr-6 lg:mt-2"
+                className="lg:mr-6 lg:mt-2"
                 width="100"
                 height="0"
                 priority
