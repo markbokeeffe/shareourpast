7852bf451104cfe10189d382983616289a463f10
marbokeeffe <mark.okeeffe@gmail.com>
2024-10-21 19:42:27 +0100

    Border

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -7,7 +7,7 @@ import Navigation from '../Navigation';
 
 export const Header = ({ children}: PropsWithChildren) => {
 	return (
-        <header className="z-50 h-[80px] font-extrabold bg-red fixed bg-[#007b8b] flex w-full lg:h-[70px] flex w-full justify-center">
+        <header className="z-50 h-[80px] font-extrabold bg-red fixed bg-[#007b8b] flex w-full lg:h-[70px] flex w-full justify-center border-b-[1px] border-[#95b3b8]">
             <div className="lg:px-4 flex items-center w-full text-white justify-between">
                 <Logo/>
                 <Navigation />
