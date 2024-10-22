093dc73c02987579b0ba3bf00ac33c3fb5be659d
marbokeeffe <mark.okeeffe@gmail.com>
2024-10-22 15:01:39 +0100

    Verify test

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -31,18 +31,18 @@ export async function POST(request: { formData: () => any; }) {
             },
           });
 
-        await new Promise((resolve, reject) => {
-            // verify connection configuration
-            transporter.verify(function (error, success) {
-                if (error) {
-                    console.log(error);
-                    reject(error);
-                } else {
-                    console.log("Server is ready to take our messages");
-                    resolve(success);
-                }
-            });
-        });
+        // await new Promise((resolve, reject) => {
+        //     // verify connection configuration
+        //     transporter.verify(function (error, success) {
+        //         if (error) {
+        //             console.log(error);
+        //             reject(error);
+        //         } else {
+        //             console.log("Server is ready to take our messages");
+        //             resolve(success);
+        //         }
+        //     });
+        // });
 
         // await new Promise((resolve,reject)=>{
 
