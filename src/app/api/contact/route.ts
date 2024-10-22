e7fca2001d2f962cef5939431646c4c115bdf631
marbokeeffe <mark.okeeffe@gmail.com>
2024-10-22 13:30:21 +0100

    Promise

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -30,6 +30,19 @@ export async function POST(request: { formData: () => any; }) {
             },
           });
 
+          await new Promise((resolve, reject) => {
+            // verify connection configuration
+            transporter.verify(function (error, success) {
+                if (error) {
+                    console.log(error);
+                    reject(error);
+                } else {
+                    console.log("Server is ready to take our messages");
+                    resolve(success);
+                }
+            });
+        });
+
 
 
           await new Promise((resolve, reject) => {
